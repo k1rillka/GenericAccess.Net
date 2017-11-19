07a5974bf6634866610cd1a4c62df68bc58c6e5f
Kirill Shingel <kshingel@gmail.com>
2017-11-19 17:57:27 +0300

    Add project files.

---
+++ GenericAccess/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericAccess
+{
+    public class Class1
+    {
+    }
+}
